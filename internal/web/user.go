e3c3b89f5db2cab3929910667f6bafe95b8d23e6
17882237881 <1091426437@qq.com>
2026-01-07 19:40:40 +0800

    优化代码格式，移除多余空白行

--- internal/web/user.go
+++ internal/web/user.go
@@ -1,24 +1,38 @@
 package web
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	regexp "github.com/dlclark/regexp2"
+	"github.com/gin-gonic/gin"
+)
 
 // UserHandler 处理用户相关的请求
 type UserHandler struct {
-	
-
+	emailExp *regexp.Regexp
+	passwordExp *regexp.Regexp
 }
 
+func NewUserHandler() *UserHandler {
+	const (
+		emailRegex    = `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
+		passwordRegex = `^.{6,16}$`
+	)
+	return &UserHandler{
+		emailExp:    regexp.MustCompile(emailRegex, regexp.None),
+		passwordExp: regexp.MustCompile(passwordRegex, regexp.None),
+	}
+}
 
-
-func(u *UserHandler) RegisterRoutes(server *gin.Engine) {
+func (u *UserHandler) RegisterRoutes(server *gin.Engine) {
 
 	ug := server.Group("/users")
-	
+
 	// 用户注册
-	ug.POST("/signup",u.Signup)
+	ug.POST("/signup", u.Signup)
 
 	// 用户登录
-	ug.POST("/login",u.Login)
+	ug.POST("/login", u.Login)
 
 	// 用户编辑
 	ug.POST("/edit", u.Edit)
@@ -27,21 +41,57 @@ func(u *UserHandler) RegisterRoutes(server *gin.Engine) {
 	ug.GET("/profile", u.Profile)
 }
 
+func (u *UserHandler) Signup(c *gin.Context) {
+	type SingUpReq struct {
+		Email           string `json:"email"`
+		ConfirmPassword string `json:"confirmPassword"`
+		Password        string `json:"password"`
+	}
 
-func(u *UserHandler) Signup(c *gin.Context) {
+	var req SingUpReq
+	if err := c.Bind(&req); err != nil { //Bind 方法会根据请求的 Content-Type 来解析你的数据到req里面
+		return
+	}
 	
+	ok, err := u.emailExp.MatchString(req.Email)
+	if err != nil {
+		c.String(http.StatusOK, "系统错误")
+		return
+	}
+	if !ok {
+		c.String(http.StatusOK, "邮箱格式不正确")
+		return
+	}
+
+	if req.Password != req.ConfirmPassword {
+		c.String(http.StatusOK, "密码不一致")
+		return
+	}
+
+	
+	ok, err = u.passwordExp.MatchString(req.Password)
+	if err != nil {
+		c.String(http.StatusOK, "系统错误")
+		return
+	}
+	if !ok {
+		c.String(http.StatusOK, "密码格式不正确")
+		return
+	}
+
+	//省略数据库操作
+
+	c.String(http.StatusOK, "注册成功")
 }
 
-func(u *UserHandler) Login(c *gin.Context) {
-	
+func (u *UserHandler) Login(c *gin.Context) {
+
 }
 
+func (u *UserHandler) Edit(c *gin.Context) {
 
-func(u *UserHandler) Edit(c *gin.Context) {
-	
 }
 
-func(u *UserHandler) Profile(c *gin.Context) {
-	
-	 
+func (u *UserHandler) Profile(c *gin.Context) {
+
 }
