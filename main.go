3b35ae836969a8fe94651a7be4da171f44c25630
17882237881 <1091426437@qq.com>
2026-01-08 11:44:44 +0800

    feat: add CORS middleware configuration

--- main.go
+++ main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"time"
 	"webook/internal/web"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	server := gin.Default()
+
+	// middleware
+	server.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"https://localhost:3000"},
+		AllowMethods: []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders: []string{"Content-Type", "authorization"},
+		AllowCredentials: true, // 允许发送 cookie
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "https://localhost:3000"
+		}, 
+		MaxAge: 12 * time.Hour,
+	}))
+
+
 	u := web.NewUserHandler()
 	u.RegisterRoutes(server) 
 	server.Run(":8080")
